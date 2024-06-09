a3ada9ad2e1aacb88bd7e7fd324782355fdbc1fd
galinabogdanova-puidokiene <galina11bp@gmail.com>
2024-06-09 21:22:08 +0300

    git

--- src/test/java/ApiTests/Test1.java
+++ src/test/java/ApiTests/Test1.java
@@ -8,12 +8,14 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 
 public class Test1 {
-    @Test
-    public void testBody90210() {
+
+    @Test(dataProvider = "zipCodesAndPlaces", dataProviderClass = Parameterized.class)
+    public void testBody90210(String countryCode, String zipCode, String expectedPlaceName) {
         given().
+                pathParam("countryCode", countryCode).pathParam("zipCode", zipCode).
                 when().
-                get("http://api.zippopotam.us/us/90210").
-                then().assertThat().body("places[0].'place name'", equalTo("Beverly Hills"));
+                get("http://api.zippopotam.us/{countryCode}/{zipCode}").
+                then().assertThat().body("places[0].'place name'", equalTo(expectedPlaceName));
     }
 
     @Test
